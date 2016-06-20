e2598e192b84d557d345c432b92d2eb8ff752763
themadhukiran <pmadhukiran96@gmail.com>
2016-06-20 20:41:59 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -287,8 +287,10 @@ public class Main {
             System.out.println("td: " + tourDetail);
             sendMessage(MessageData.withMessage(user, "Here are some details about " + tourDetail.name + "\n\n" + "Summary: \n" + tourDetail.summary));
             Thread.sleep(200);
+            sendMessage(MessageData.withMessage(user, "\n\n Highlights: \n" + tourDetail.highlights));
+            Thread.sleep(200);
             sendMessage(MessageData.withAttachment(user, Attachment.withButtons(ButtonsPayload.create(
-                    "Highlights: \n " + tourDetail.highlights,
+                    "Wanna book?",
                     RedirectButton.create("Book", HeadoutApi.WEBSITE_BASE_URL + "/tour/" + tourDetail.id)
             ), gson)));
         } else {
